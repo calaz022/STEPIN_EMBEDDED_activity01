38e0bbb1e56f21208c26c0565ac9aaa451f749ef
suhas kn <89761761+calaz022@users.noreply.github.com>
2021-09-16 23:16:13 +0530

    Update A2.h

--- 03_Implementation/inc/A2.h
+++ 03_Implementation/inc/A2.h
@@ -1,1 +1,25 @@
+/**
+*@file A2.h
+* @author Suhas kn
+* @brief Header file for activity1
+* @version 0.1
+* @date 2021-04-27
+*/
+
+#ifndef A2_H_INCLUDED
+#define A2_H_INCLUDED
+
+#include <util/delay.h>
+#include <avr/io.h>
+
+
+void InitADC();
+
+
+uint16_t ReadADC(uint8_t ch);
+
+
+uint16_t activity2_GetADC(void);
+
+#endif
 
