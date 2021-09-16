e594304875e6d19a597b2d8024c790309c9e6676
suhas kn <89761761+calaz022@users.noreply.github.com>
2021-09-16 23:14:29 +0530

    Update A1.h

--- 03_Implementation/inc/A1.h
+++ 03_Implementation/inc/A1.h
@@ -1,1 +1,14 @@
+/**
+*@file A1.h
+* @author Suhas kn
+* @brief Header file for activity1
+* @version 0.1
+* @date 2021-04-27
 
+*/
+#ifndef A1_H_INCLUDED
+#define A1_H_INCLUDED
+
+void A1(void);
+
+#endif 
